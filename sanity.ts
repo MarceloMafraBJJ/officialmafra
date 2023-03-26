986317926488d6bb30891fdfdd4c87a5e771f303
marcelomafrabjj <marcelo.mafrademoura@gmail.com>
2023-03-26 00:02:30 -0300

    update

--- sanity.ts
+++ sanity.ts
@@ -3,7 +3,7 @@ import imageUrlBuilder from "@sanity/image-url";
 
 export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID! || "actts3f8",
   apiVersion: "2021-10-21",
   useCdn: process.env.NODE_ENV === "production",
 };
